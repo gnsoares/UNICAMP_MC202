35c9c4603725af388c7c2ae52c8c9e81de3cde9a
Gustavo Soares <gnsoares@localhost.localdomain>
2018-10-14 18:44:37 -0300

    lab10

--- lab10/arvore.c
+++ lab10/arvore.c
@@ -5,15 +5,16 @@
 
 NodeT * create_tree(char *c, NodeT *left, NodeT *right) {
 	NodeT *root = malloc(sizeof(NodeT));
-	if (!root)
-		exit(1);
+	if (!root) exit(1);
 	strcpy(root->key, c);
 	root->left = left;
 	root->right = right;
 	return root;
 }
 
-int inner_operator_pos(char **expression, int beg, int end) {
+/* Funcao que acha e devolve a posicao do operador
+ * de uma expressao matematica */
+int operator_pos(char **expression, int beg, int end) {
 	int i, is_beg, is_end;
 
 	is_beg = expression[beg+2][0] == '+' || expression[beg+2][0] == '-'
@@ -21,14 +22,18 @@ int inner_operator_pos(char **expression, int beg, int end) {
 	is_end = expression[end-2][0] == '+' || expression[end-2][0] == '-'
 		  || expression[end-2][0] == '*' || expression[end-2][0] == '/';
 
+	/* Caso o primeiro operando seja um numero */
 	if (is_beg)
 		return beg + 2;
+	/* Caso o segundo operando seja um numero */
 	else if (is_end)
 		return end - 2;
+	/* Caso ambos operandos sejam expressoes */
 	else
 		for (i = beg+1; i < end; i++)
 			if ((expression[i-1][0] == ')') && (expression[i+1][0] == '('))
 				return i;
+
 	return 0;
 }
 
@@ -36,13 +41,16 @@ NodeT * expression_tree(char **expression, int beg, int end) {
 	int pos;
 	NodeT *tree, *left, *right;
 
+	/* Caso seja uma expressao com mais de um termo */
 	if (expression[beg][0] == '(') {
-		pos = inner_operator_pos(expression, beg, end);
+		pos = operator_pos(expression, beg, end);
 		left = expression_tree(expression, beg+1, pos-1);
 		right = expression_tree(expression, pos+1, end-1);
 		tree = create_tree(expression[pos], left, right);
+	/* Caso a expressao possua apenas um termo */
 	} else if (beg == end) {
 		tree = create_tree(expression[beg], NULL, NULL);
+	/* Caso nao seja uma expressao valida */
 	} else {
 		return NULL;
 	}
@@ -50,6 +58,7 @@ NodeT * expression_tree(char **expression, int beg, int end) {
 	return tree;
 }
 
+/* Funcao que verifica se um no contem um numero */
 int node_is_number(NodeT *root) {
 	if (root)
 		return (root->key[0] >= '0' && root->key[0] <= '9')
@@ -57,6 +66,7 @@ int node_is_number(NodeT *root) {
 	return 0;
 }
 
+/* Funcao que verifica se um no contem um operador */
 int node_is_operator(NodeT *root) {
 	if (root)
 		return (root->key[0] == '+' || root->key[0] == '-'
@@ -68,9 +78,14 @@ int node_is_operator(NodeT *root) {
 NodeT * simplify(NodeT *root) {
 	int result;
 	if (root) {
+		/* As subarvores devem estar simplificadas */
 		root->left = simplify(root->left);
 		root->right = simplify(root->right);
+
+		/* Se os dois filhos sao numeros, simplificar */
 		if (node_is_number(root->left) && node_is_number(root->right)) {
+
+			/* Definicao da operacao */
 			if (root->key[0] == '+')
 				result = atoi(root->left->key) + atoi(root->right->key);
 			else if (root->key[0] == '-')
@@ -79,6 +94,8 @@ NodeT * simplify(NodeT *root) {
 				result = atoi(root->left->key) * atoi(root->right->key);
 			else if (root->key[0] == '/')
 				result = atoi(root->left->key) / atoi(root->right->key);
+
+			/* Mudanca dos valores e remocao das subarvores */
 			sprintf(root->key, "%d", result);
 			free_tree(root->left);
 			free_tree(root->right);
@@ -89,6 +106,10 @@ NodeT * simplify(NodeT *root) {
 	return root;
 }
 
+/* Funcao auxiliar para impressao da expressao;
+ * Imprime caso no seja operando;
+ * Chama a funcao principal caso no seja expressao;
+ */
 void print_aux(NodeT *root) {
 	if (node_is_operator(root))
 		print_expression(root);
