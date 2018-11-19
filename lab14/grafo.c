9a001b7624d2db4295c27c2bd08680b907f9b8dc
Gustavo Soares <gnsoares@localhost.localdomain>
2018-11-19 17:30:59 -0200

    falta comment

---
+++ lab14/grafo.c
@@ -0,0 +1,76 @@
+#include "grafo.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+list_node_t *list_add(list_node_t *list, int x) {
+	list_node_t *new = malloc(sizeof(list_node_t));
+	if (!new) exit(1);
+	new->value = x;
+	new->next = list;
+	return new;
+}
+
+list_node_t *list_remove(list_node_t *list, int x) {
+	list_node_t *next;
+	if (!list) return NULL;
+	if (list->value == x) {
+		next = list->next;
+		free(list);
+		return next;
+	}
+	list = list_remove(list->next, x);
+	return list;
+}
+
+void list_free(list_node_t *list) {
+	if (!list) return;
+	list_free(list->next);
+	free(list);
+}
+
+graph_t *graph_create(int n_v) {
+	int i;
+	graph_t *graph = malloc(sizeof(graph_t));
+	if (!graph) exit(1);
+	graph->n_v = n_v;
+	graph->adjacency = malloc(n_v * sizeof(list_node_t *));
+	if (!graph->adjacency) exit(1);
+	for (i = 0; i < n_v; i++)
+		graph->adjacency[i] = NULL;
+	return graph;
+}
+
+void graph_edge_insert(graph_t *graph, int u, int v) {
+	graph->adjacency[u] = list_add(graph->adjacency[u], v);
+	graph->adjacency[v] = list_add(graph->adjacency[v], u);
+}
+
+void graph_edge_remove(graph_t *graph, int u, int v) {
+	graph->adjacency[u] = list_remove(graph->adjacency[u], v);
+	graph->adjacency[v] = list_remove(graph->adjacency[v], u);	
+}
+
+int has_edge(graph_t *graph, int u, int v) {
+	list_node_t *curr;
+	for (curr = graph->adjacency[u]; curr; curr = curr->next)
+		if (curr->value == v)
+			return 1;
+	return 0;
+}
+
+void print_edges(graph_t *graph) {
+	int u;
+	list_node_t *t;
+	for (u = 0; u < graph->n_v; u++)
+		for (t = graph->adjacency[u]; t != NULL; t = t->next)
+			printf("{%d,%d}\n", u, t->value);
+}
+
+void graph_free(graph_t *graph) {
+	int i;
+	for (i = 0; i < graph->n_v; i++)
+		list_free(graph->adjacency[i]);
+	free(graph->adjacency);
+	free(graph);
+}
+
---
+++ lab14/grafo.h
@@ -0,0 +1,23 @@
+#ifndef GRAFO_H
+#define GRAFO_H
+
+typedef struct list_node_s {
+	int value;
+	struct list_node_s *next;
+} list_node_t;
+
+typedef struct graph_s {
+	list_node_t **adjacency;
+	int n_v;
+} graph_t;
+
+list_node_t *list_add(list_node_t *list, int x);
+void list_free(list_node_t *list);
+graph_t *graph_create(int n_v);
+void graph_edge_insert(graph_t *graph, int u, int v);
+void graph_edge_remove(graph_t *graph, int u, int v);
+int has_edge(graph_t *graph, int u, int v);
+void print_edges(graph_t *graph);
+void graph_free(graph_t *graph);
+
+#endif
