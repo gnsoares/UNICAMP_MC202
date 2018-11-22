6b9f8d1c04dbaee80bf980937ddbeefa4e7262e6
Gustavo Soares <gnsoares@localhost.localdomain>
2018-11-22 17:32:36 -0200

    lab15

--- lab15/grafo.c
+++ lab15/grafo.c
@@ -3,84 +3,46 @@
 #include <limits.h>
 #include "grafo.h"
 
-list_node_t *list_add(list_node_t *list, int x) {
-	list_node_t *new = malloc(sizeof(list_node_t));
-	if (!new) exit(1);
-	new->value = x;
-	new->next = list;
-	return new;
-}
-
-list_node_t *list_remove(list_node_t *list, int x) {
-	list_node_t *next;
-	if (!list) return NULL;
-	if (list->value == x) {
-		next = list->next;
-		free(list);
-		return next;
-	}
-	list = list_remove(list->next, x);
-	return list;
-}
-
-void list_free(list_node_t *list) {
-	if (!list) return;
-	list_free(list->next);
-	free(list);
-}
-
 graph_t *graph_create(int n_v) {
-	int i;
+	int i, j;
 	graph_t *graph = malloc(sizeof(graph_t));
 	if (!graph) exit(1);
 	graph->n_v = n_v;
-	graph->adj = malloc(n_v * sizeof(list_node_t *));
+	graph->adj = malloc(n_v * sizeof(int *));
 	if (!graph->adj) exit(1);
+	for (i = 0; i < n_v; i++) {
+		graph->adj[i] = malloc(n_v *sizeof(int));
+		if (!graph->adj[i]) exit(1);
+	}
 	for (i = 0; i < n_v; i++)
-		graph->adj[i] = NULL;
+		for (j = 0; j < n_v; j++)
+			graph->adj[i][j] = 0;
 	return graph;
 }
 
 void graph_edge_insert(graph_t *graph, int u, int v) {
-	if (has_edge(graph, u, v)) return;
-	graph->adj[u] = list_add(graph->adj[u], v);
-	graph->adj[v] = list_add(graph->adj[v], u);
-}
-
-void graph_edge_remove(graph_t *graph, int u, int v) {
-	graph->adj[u] = list_remove(graph->adj[u], v);
-	graph->adj[v] = list_remove(graph->adj[v], u);	
-}
-
-int has_edge(graph_t *graph, int u, int v) {
-	list_node_t *curr;
-	if (u == v) return 1;
-	for (curr = graph->adj[u]; curr; curr = curr->next)
-		if (curr->value == v)
-			return 1;
-	return 0;
-}
-
-void print_edges(graph_t *graph) {
-	int u;
-	list_node_t *t;
-	for (u = 0; u < graph->n_v; u++)
-		for (t = graph->adj[u]; t != NULL; t = t->next)
-			printf("{%d,%d}\n", u, t->value);
+	graph->adj[u][v] = graph->adj[v][u] = 1;
 }
 
 void graph_free(graph_t *graph) {
 	int i;
 	for (i = 0; i < graph->n_v; i++)
-		list_free(graph->adj[i]);
+		free(graph->adj[i]);
 	free(graph->adj);
 	free(graph);
 }
+
+/* Implementacao de uma fila usando lista ligada a ser usada no percurso em largura */
+typedef struct list_node_s {
+	int value;
+	struct list_node_s *next;
+} list_node_t;
 
 typedef struct queue_s {
 	list_node_t *head, *tail;
 } queue_t;
 
+/* Criacao da fila */
 queue_t *queue_create(void) {
 	queue_t *new = malloc(sizeof(queue_t));
 	if (!new) exit(1);
@@ -88,130 +50,70 @@ queue_t *queue_create(void) {
 	return new;
 }
 
+/* Verificacao se a fila esta vazia */
 int queue_is_empty(queue_t *queue) {
 	return !queue->head;
 }
 
+/* Adicao de elemento na fila */
 void queue_queue(queue_t *queue, int x) {
 	list_node_t *new = malloc(sizeof(list_node_t));
 	if (!new) exit(1);
 	new->value = x;
 	new->next = NULL;
-	if (queue->tail) queue->tail->next = new;
-	else queue->head = new;
+	if (queue_is_empty(queue)) queue->head = new;
+	else queue->tail->next = new;
 	queue->tail = new;
 }
 
+/* Remocao de elemento da fila */
 int queue_dequeue(queue_t *queue) {
 	int r;
 	list_node_t *node;
-	if (!queue->head) return -1;
 	r = queue->head->value;
 	node = queue->head;
 	if (queue->head == queue->tail) queue->tail = NULL;
 	queue->head = queue->head->next;
-	free(node); 
+	free(node);
 	return r;
 }
 
-void list_print(list_node_t *list) {
-	if (list) {
-		printf("%d ", list->value);
-		list_print(list->next);
-	}
-}
-
-void print_queue(queue_t *queue) {
-	if (queue->head) {
-		printf("fila: ");
-		list_print(queue->head);
-	}
-	printf("\n");
-}
-
+/* Liberacao do espaco alocado pela fila */
 void queue_free(queue_t *queue) {
-	while (queue->head)
+	while (!queue_is_empty(queue))
 		queue_dequeue(queue);
 	free(queue);
 }
 
-/*int graph_bfs(graph_t *graph, int **mat, int source, int destination) {
-	int i, found = 0, len = 0;
-	int *visit = malloc(graph->n_v * sizeof(int));
-	int *dist = malloc(graph->n_v * sizeof(int));
-	list_node_t *curr;
+/* Percurso em largura do grafo:
+	@param: graph - estrutura do grafo (usando matriz de adjacencias) 
+	@param: src - posicao inicial do percurso
+	@param: dist - vetor em que cada posicao e a menor distancia da
+				   origem a essa posicao
+*/
+void graph_bfs(graph_t *graph, int src, int *dist) {
+	int i, j;
+	int *visited = malloc(graph->n_v * sizeof(int));
 	queue_t *queue = queue_create();
-	
+
 	for (i = 0; i < graph->n_v; i++) {
-		visit[i] = 0;
-		dist[i] = 98;
+		visited[i] = 0;
+		dist[i] = -1;
 	}
-	dist[source] = 0;
-	queue_queue(queue, source);
+
+	queue_queue(queue, src);
+	visited[src] = 1;
+	dist[src] = 0;
 	while (!queue_is_empty(queue)) {
 		i = queue_dequeue(queue);
-		if (i == destination) {
-			found = 1;
-		}
-		if (visit[i] == 1)
-			continue;
-		visit[i] = 1;
-		for (curr = graph->adj[i]; curr; curr = curr->next) {
-			if (dist[curr->value] == 98 || dist[curr->value] > dist[i] + 1) dist[curr->value] = dist[i] + 1;
-			queue_queue(queue, curr->value);
-		}
+		for (j = 0; j < graph->n_v; j++)
+			if (graph->adj[i][j] && !visited[j]) {
+				visited[j] = 1;
+				dist[j] = dist[i] + 1;
+				queue_queue(queue, j);
+			}
 	}
 
 	queue_free(queue);
-	len = dist[destination];
-	free(visit); free(dist);
-	print_mat(mat, graph->n_v); printf("\n");
-	if (found) return len;
-	return 0;
-}*/
-
-void graph_bfs(graph_t *graph, int s) {
-	int w, v;
-	int *pai = malloc(graph->n_v * sizeof(int));
-	int *visitado = malloc(graph->n_v * sizeof(int));
-	queue_t *f = queue_create();
-	list_node_t *node;
-
-	for (v = 0; v < graph->n_v; v++) {
-		pai[v] = -1;
-		visitado[v] = 0;
-	}
-
-	queue_queue(f,s);
-	pai[s] = s;
-	visitado[s] = 1;
-	while (!queue_is_empty(f)) {
-		v = queue_dequeue(f);
-		for (w = 0, node = graph->adj[v]; w < graph->n_v && node; w++, node = node->next)
-			if (node && !visitado[w]) {
-				visitado[w] = 1;/* evita repetição na fila */
-				pai[w] = v;
-				queue_queue(f, w);
-			}
-	}
-	queue_free(f);
-	free(visitado);
-	for (v = 0; v < graph->n_v; v++) {
-		if (pai[v] != -1)
-			printf("%d ", pai[v]);
-	}
-	printf("\n");
-	/*return pai;*/
-}
-
-void print_mat(int **mat, int size) {
-	int i, j;
-	for (i = 0; i < size; i++) {
-		for (j = 0; j < size; j++)
-			if (mat[i][j] > 9)
-				printf("%d|", mat[i][j]);
-			else
-				printf(" %d|", mat[i][j]);
-		printf("\n");
-	}
+	free(visited);
 }
--- lab15/lab15.c
+++ lab15/lab15.c
@@ -3,26 +3,32 @@
 #include <limits.h>
 #include "grafo.h"
 
-#define DEBUG 1
+/* Verificacao se um elemento esta em um vetor */
+int in(int *v, int v_size, int x) {
+	int i;
+	for (i = 0; i < v_size; i++)
+		if (v[i] == x)
+			return 1;
+	return 0;
+}
 
-int main()
-{
-	int i, j, k, n_students, n_groups, *v, **links;
-	graph_t *groups_graph;
+/* Busca pelo maior elemento de um vetor */
+int max(int *v, int v_size) {
+	int i, max = v[0];
+	for (i = 1; i < v_size; i++)
+		if (v[i] > max)
+			max = v[i];
+	return max;
+}
 
+int main() {
+	int i, j, k, n_students, n_groups, *v;
+	graph_t *graph;
+
+	/* Leitura e construcao das estruturas usadas */
 	scanf("%d %d", &n_students, &n_groups);
-	groups_graph = graph_create(n_students);
-	links = malloc(n_students * sizeof(int *));
-	if (!links) exit(1);
-	for (i = 0; i < n_students; i++) {
-		links[i] = malloc(n_students * sizeof(int));
-		if (!links[i]) exit(1);
-	}
-	for (i = 0; i < n_students; i++)
-		for (j = 0; j < n_students; j++)
-			links[i][j] = (i == j) ? 1 : 98;
-	printf("n = %d\n", n_students);
-
+	graph = graph_create(n_students);
+	/* Insercao das arestas do grafo */
 	v = malloc(n_students * sizeof(int));
 	if (!v) exit(1);
 	for (i = 0; i < n_groups; i++) {
@@ -30,40 +36,22 @@ int main()
 		for (j = 0; j < n_students; j++)
 			scanf("%d", &v[j]);
 		for (j = 0; j < n_students; j++)
-			for (k = j+1; k < n_students; k++)
-				graph_edge_insert(groups_graph, v[j], v[k]);
-	}
-		
-	graph_bfs(groups_graph, 0);
-	/*print_mat(links, groups_graph->n_v); printf("\n");
+			for (k = j; k < n_students; k++)
+				graph_edge_insert(graph, v[j], v[k]);
+	}	
 
-	for (i = 0; i < groups_graph->n_v; i++)
-		for (j = i+1; j < groups_graph->n_v; j++)
-			links[j][i] = links[i][j] = graph_bfs(groups_graph, links, i, j);
-		
-	print_mat(links, groups_graph->n_v); printf("\n");*/
-
-	/*for (i = 0; i < groups_graph->n_v-1; i++)
-		for (j = i+2; j < groups_graph->n_v; j++)
-			links[i][j] = links[j][i] = (links[i][j-1] && links[j-1][j]) || (!links[i][j-1] && !links[j-1][j]) ? 98 : 0;
-
-	print_mat(links, groups_graph->n_v); printf("\n");
-
-	for (i = 0; i < groups_graph->n_v; i++)
-		for (j = 0; j < groups_graph->n_v; j++)
-			if ((i - j > 1 || i - j < -1) && links[i][j])
-				graph_bfs(groups_graph, links, i, j);
-		
-	print_mat(links, groups_graph->n_v); printf("\n");*/
-
-/*	for (i = 0; i < groups_graph->n_v; i++) {
-		for (j = 0; j < groups_graph->n_v; j++)
-			if (links[i][j] == 0)
+	/* Obtencao das distancias e dos caminhos possiveis juntamente com a impressao */
+	for (i = 0; i < graph->n_v; i++) {
+		graph_bfs(graph, i, v);
+		for (j = 0; j < graph->n_v; j++)
+			if (v[j] == -1)
 				printf("%d ", j);
-		printf("\n");
+		printf("- %d\n", max(v, graph->n_v));
 	}
-*/
+
+	/* Liberacao do espaco alocado */
 	free(v);
-	printf("rodou\n");
+	graph_free(graph);
+
 	return 0;
 }
